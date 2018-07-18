c38f32246747e07d82a2be34756da915ea03b5e4
Sebastian Ritter <bastie@users.noreply.github.com>
2018-07-18 22:36:44 +0200

    Create stdio.java

---
+++ src/main/java/lang/c/stdio.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018, Sͬeͥbͭaͭsͤtͬian.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+ 
+package lang.c;
+ 
+/**
+ * @author Sͬeͥbͭaͭsͤtͬian
+ *
+ */
+public class stdio {
+
+  /**
+   * indicate end-of-file conditions
+   */
+  public final static int EOF = -1;
+}
