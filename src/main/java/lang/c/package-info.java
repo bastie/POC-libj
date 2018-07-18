327a6727fca096db8734dc3ea6876c9cda2962fb
Sebastian Ritter <bastie@users.noreply.github.com>
2018-07-18 22:13:31 +0200

    Create package-info.java

---
+++ src/main/java/lang/c/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018, Sͬeͥbͭaͭsͤtͬian.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+ 
+/**
+ * @author Sͬeͥbͭaͭsͤtͬian
+ *
+ */
+package lang.c;
